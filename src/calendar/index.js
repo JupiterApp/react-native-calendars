120711ec0c2bf950bb399f15d47036b99d23a7e0
Inseo B <yunibaek@hotmail.com>
2020-04-28 13:17:53 -0400

    update how today gets calculated

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -167,7 +167,11 @@ class Calendar extends Component {
     } else if ((minDate && !dateutils.isGTE(day, minDate)) || (maxDate && !dateutils.isLTE(day, maxDate))) {
       state = 'disabled';
     } else if (!dateutils.sameMonth(day, this.state.currentMonth)) {
-      state = '_disabled';
+      if (dateutils.sameDate(day, XDate())) {
+        state = 'today';
+      } else {
+        state = '_disabled';
+      }
     } else if (dateutils.sameDate(day, XDate())) {
       state = 'today';
     }
